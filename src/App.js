3f5c91b37201463124d1d7f5bf352c4885ee7205
Jane <maryjanesalem@outlook.ph>
2023-02-03 14:28:26 +0800

    added NotFound page

--- src/App.js
+++ src/App.js
@@ -5,21 +5,20 @@ import Footer from './components/Footer';
 import {Routes, Route} from 'react-router-dom'
 import Categories from './pages/Categories';
 import About from './pages/About';
+import NotFound from './pages/NotFound';
+import MainLayout from './pages/layout/MainLayout';
 
 function App() {
     return (
         <>
-
-            <Header />
                 <Routes>
-                
-                    <Route path="/" element={<Main />}></Route>
-                    <Route path="/categories" element={<Categories />}></Route>
-                    <Route path="/about" element={<About />}></Route>
-
+                    <Route element={<MainLayout />}>
+                        <Route path="/" element={<Main />}></Route>
+                        <Route path="/categories" element={<Categories />}></Route>
+                        <Route path="/about" element={<About />}></Route>
+                    </Route>
+                    <Route path='*' element={<NotFound />} />
                 </Routes>
-            <Footer />
-
         </>
     );
 }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,23 @@
 
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
+    return (
+        <>
+            <div className="container pt-5">
+
+                <h1 className="text-center mt-5">Page not found</h1>
+                <p className="text-center"><em>The requested URL was not found on this server</em></p>
+                <div className="text-center mt-5">
+                    <img src="./img/error.jpg" width={800}></img>
+                </div>
+                <div className="text-center mt-5">
+                    <button type="button" class="btn btn-primary mx-2">GO HOME</button>
+                    <button type="button" class="btn btn-secondary mx-2">CONTACT US</button>
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default NotFound
