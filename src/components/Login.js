7e01075bdea3db9647f0b4b9cf198cd41b64fa0c
Jane <maryjanesalem@outlook.ph>
2023-02-05 21:57:40 +0800

    added padding for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,9 @@
 const Login = () => {
     return (
         <div className="login">
-            <div className="card text-white bg-primary mb-4">
+            <div className="card text-white bg-primary mb-4 p-4">
                 <div className="card-body">
-                    <h4 className="card-title text-center mt-3 fw-bolder">Login</h4>
+                    <h4 className="card-title text-center  fw-bolder">Login</h4>
                     <div className="card-text">
                         <div className="form-group">
                             <fieldset className="login-field">
@@ -12,7 +12,7 @@ const Login = () => {
                                 <input className="form-control" id="username" type="text" placeholder="Username" />
                                 <label className="form-label mt-4" htmlFor="password">Password:</label>
                                 <input className="form-control" id="password" type="text" placeholder="Password" />
-                                <div className="loginbuttons mb-2">
+                                <div className="loginbuttons">
                                     <button type="button" className="btn btn-secondary btn-login">Login</button>
                                     <button type="button" className="btn btn-warning btn-signup">Sign-up</button>
                                 </div>
